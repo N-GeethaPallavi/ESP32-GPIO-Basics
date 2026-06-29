3863e315ac61935d11437cdbe1e253366443faad
N-GeethaPallavi <geethapallavi696@gmail.com>
2026-06-29 19:57:23 +0530

    Update ESP32-Dual-LED-Blink.ino
    Configured two external LEDs on separate GPIO pins to flash simultaneously using simple software delays, establishing fundamental digital output control on the ESP32.

--- ESP32-Dual-LED-Blink.ino
+++ ESP32-Dual-LED-Blink.ino
@@ -2,15 +2,15 @@
 # define LED_2 5 
 void setup ()  
 { 
-} 
 pinMode(LED_1,OUTPUT); 
 pinMode(LED_2,OUTPUT); 
+}
 void loop()  
 { 
 digitalWrite(LED_1,HIGH); 
 digitalWrite(LED_2,LOW); 
 delay(500); 
-  digitalWrite(LED_1,LOW); 
-  digitalWrite(LED_2,HIGH); 
-  delay(500); 
+digitalWrite(LED_1,LOW); 
+digitalWrite(LED_2,HIGH); 
+delay(500); 
 }
