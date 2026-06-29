d5ae3a2965d56a0918484ff8ec0f81ac34c7aaf5
N-GeethaPallavi <geethapallavi696@gmail.com>
2026-06-29 19:45:20 +0530

    Create ESP32-Pulldown-Control.ino
    Configured digital input using the internal pin states and software logic on the ESP32 to safely read the button press without using external physical resistors on the breadboard.

---
+++ ESP32-Pulldown-Control.ino
@@ -0,0 +1,20 @@
+# define pushbutton 13 
+# define LED 2  
+void setup ()  
+{ 
+} 
+Serial.begin(115200); 
+pinMode(LED,OUTPUT); 
+pinMode(pushbutton,INPUT); 
+void loop ()  
+{ 
+} 
+if (digitalRead(pushbutton) == HIGH ) 
+{ 
+} 
+digitalWrite(LED,HIGH); 
+Serial.println("Pressed"); 
+else  
+{ 
+} 
+digitalWrite(LED,LOW);
